88e41fcbd2a88e8b640d47b305672087ffc39453
deckerth <deckerth@gmail.com>
2019-06-16 11:42:21 +0200

    Projektdateien hinzufügen.

---
+++ MathEvaluatorTest/MainPage.xaml.vb
@@ -0,0 +1,9 @@
+﻿' Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+''' <summary>
+''' Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+End Class
